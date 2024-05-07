e6530b31cf3c43296516639f7cd842002eedbbf4
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-05-07 07:23:28 -0400

    Refactor Paddle.lua for improved code organization and readability

--- Paddle.lua
+++ Paddle.lua
@@ -1,21 +1,28 @@
+-- Paddle class definition using hump.class
 Paddle = Class {}
 
+-- Initialization of the Paddle
 function Paddle:init(x, y, width, height)
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    self.dy = 0
+    self.x = x           -- The X coordinate
+    self.y = y           -- The Y coordinate
+    self.width = width   -- Width of the paddle
+    self.height = height -- Height of the paddle
+    self.dy = 0          -- Initial velocity along Y-axis
 end
 
+-- Update function to move the paddle
 function Paddle:update(dt)
     if self.dy < 0 then
+        -- Move up, ensuring the paddle does not go above the screen
         self.y = math.max(0, self.y + self.dy * dt)
     else
+        -- Move down, ensuring the paddle does not go below the screen
         self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
     end
 end
 
+-- Render function to draw the paddle on the screen
 function Paddle:render()
+    -- Draw a rectangle representing the paddle
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
